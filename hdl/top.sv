be3a1370b73b566d54ae1609c31ba360e2ce2a70
sabitov marat <sabet19997@gmail.com>
2023-05-15 18:37:54 +0700

    fir

--- hdl/top.sv
+++ hdl/top.sv
@@ -72,6 +72,15 @@ QPSK QPSK (
   .o_valid(o_valid_fir_filter)
 );
 
+fir_filter your_instance_name (
+  .aclk(aclk),                              // input wire aclk
+  .s_axis_data_tvalid(s_axis_data_tvalid),  // input wire s_axis_data_tvalid
+  .s_axis_data_tready(s_axis_data_tready),  // output wire s_axis_data_tready
+  .s_axis_data_tdata(s_axis_data_tdata),    // input wire [31 : 0] s_axis_data_tdata
+  .m_axis_data_tvalid(m_axis_data_tvalid),  // output wire m_axis_data_tvalid
+  .m_axis_data_tdata(m_axis_data_tdata)    // output wire [63 : 0] m_axis_data_tdata
+);
+
 assign data = o_data_fir_filter;
 assign o_valid_output = o_valid_fir_filter;
 
